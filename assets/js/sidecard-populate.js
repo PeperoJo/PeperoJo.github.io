80049ed64ab54fcd7fb29a7492b2bcffa5cbd1b8
Joseph Han <hanjoseph11@gmail.com>
2022-04-06 14:41:05 -0400

    side project

--- assets/js/sidecard-populate.js
+++ assets/js/sidecard-populate.js
@@ -2,12 +2,12 @@ var URL = "https://josephhan.io/assets/js/sidecard-data.json";
 
 // https://stackoverflow.com/questions/31763022/how-do-you-get-responsetext-from-jquery-getjson-method-in-javascript
 var jsonData;
-var jsonCall = $.getJSON(URL,function(){
-   jsonData = JSON.parse(jsonCall.responseText); //must be done here because it is asynchronous
+var jsonCall2 = $.getJSON(URL,function(){
+   jsonData2 = JSON.parse(jsonCall2.responseText); //must be done here because it is asynchronous
    // console.log("within call:");
-   console.log(jsonCall);
-   console.log(jsonData.projects);
-   populate(jsonData.projects)
+   console.log(jsonCall2);
+   console.log(jsonData2.projects);
+   populateSide(jsonData2.projects)
   });
 //doesn't work here
 // console.log("after call:");
@@ -29,9 +29,9 @@ function sidecardContainer(block) {
         style="
           width: 100%;
           height: 200px;
-          background-color: #F2F3F5;
+          background-color: ${this.info.projectImg.bgcolor};
         ">
-        <img src="assets/img/projects/moonranger2.png"
+        <img src="${this.info.projectImg.src}"
         style="
         display: block;
           width: 100%;
@@ -46,9 +46,20 @@ function sidecardContainer(block) {
       <div class="sideinfo p-3"
         style="
           background-color: red;
+          display: block;
+          white-space: normal;
         "
       >
-        hello
+        <div class="project2-tags">
+          ${this.info.projectTags}
+        </div>
+
+        <div class="project2-title">
+          ${this.info.projectTitle}
+        </div>
+        <div class="project2-descr">
+          ${this.info.projectDescription}
+        </div>
       </div>
     </div>
 
@@ -56,7 +67,7 @@ function sidecardContainer(block) {
   `;
 }
 
-function populate(data) {
+function populateSide(data) {
   for (i = 0; i < data.length; i++) {
     var project00 = new sidecardContainer(data[i]);
     if (project00.info.show) {
